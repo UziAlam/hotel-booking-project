57f0de50744d355b42ce3fe9165e05c8d511fb53
Uzi Alam <u.alam@live.com.au>
2026-03-18 08:58:19 +0100

    Up to date

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import HeroSection from '../components/HeroSection'
+import FeaturedDestination from '../components/FeaturedDestination'
 
 const Home = () => {
   return (
     <>
       <HeroSection/>
+      <FeaturedDestination/>
     </>
   )
 }
